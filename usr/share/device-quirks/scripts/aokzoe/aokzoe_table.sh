f5b8442426d3d5c07bae338e5a4e50d752a818d9
Jerrod Frost <13701401+jcfrosty@users.noreply.github.com>
2022-12-10 18:22:58 +0000

    add dummy files for other devices

--- usr/share/device-quirks/scripts/aokzoe/aokzoe_table.sh
+++ usr/share/device-quirks/scripts/aokzoe/aokzoe_table.sh
@@ -6,7 +6,7 @@ BOARD_NAME="$(cat /sys/devices/virtual/dmi/id/board_name)"
 CPU_NAME="$(cat /proc/cpu | awk 'NR==5 {print $4$5$6$7}')"
 
 
-# AOKZOE Devices
+### AOKZOE Devices
 if [[ "AOKZOE A1 AR07" == "$PRODUCT_NAME" ]] && [[ "AOKZOE A1 AR07" == "$BOARD_NAME"]] && [[ "AMDRyzen76800U" == "$CPU_NAME"]]; then
     echo "AOKZOE A1 FOUND"
     ./aokzoea1ar07.sh
