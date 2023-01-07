a9c2d37b6cff86e9921eae993b888b88924cd965
Derek J. Clark <derekjohn.clark@gmail.com>
2023-01-06 16:06:29 -0800

    Common sense defaults

--- usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
+++ usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
@@ -5,16 +5,11 @@ BOARD_NAME="$(cat /sys/devices/virtual/dmi/id/board_name)"
 CPU_NAME="$(cat /proc/cpuinfo | awk 'NR==5 {print $4$5$6$7}')"
 
 BOARD_LIST_2021="AYA NEO Founder:AYA NEO 2021:AYANEO 2021:AYANEO 2021 Pro:AYANEO 2021 Pro Retro Power"
-BOARD_LIST_AIR="AIR:AIR Pro"
 CPU_LIST_2021="AMDRyzen54500U:AMDRyzen74800U"
-CPU_LIST_AIR="AMDRyzen5560U:AMDRyzen75825U"
 PRODUCT_LIST_2021="AYA NEO Founder:AYA NEO 2021:AYANEO 2021:AYANEO 2021 Pro:AYANEO 2021 Pro Retro Power"
-PRODUCT_LIST_AIR="AIR:AIR Pro"
 
 if [[ ":$PRODUCT_LIST_2021:" =~ "$PRODUCT_NAME" ]] && [[ ":$BOARD_LIST_2021:" =~ "$BOARD_NAME" ]] && [[ ":$CPU_LIST_2021:" =~ "$CPU_NAME" ]]; then
   $DQ_PATH/ayaneo/2021/2021.sh
-#elif [[ ":$LIST_AIR:" =~ "$PRODUCT_NAME" ]] && [[ ":$LIST_AIR:" =~ "$BOARD_NAME" ]] && [[ ":$CPU_LIST_AIR:" =~ "$CPU_NAME" ]]; then
-#  $DQ_PATH/ayaneo/air/air.sh
 else
   echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
 fi
