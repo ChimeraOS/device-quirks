f9e401e6301cc6769361263771e7fccce5a0b312
Derek J. Clark <derekjohn.clark@gmail.com>
2023-02-10 10:08:38 -0800

    Fix id-device script

--- usr/share/device-quirks/scripts/gpd/gpd_table.sh
+++ usr/share/device-quirks/scripts/gpd/gpd_table.sh
@@ -5,18 +5,16 @@ BOARD_NAME="$(cat /sys/devices/virtual/dmi/id/board_name)"
 
 # Win3
 BOARD_WIN3="G1618-03"
-PRODUCT_WIN3="GPD"
 
 # WinMax2
 BOARD_WM2="G1619-04"
-PRODUCT_WM2="GPD"
 
 # Win3
-if [[ ":$PRODUCT_WIN3:" == "$PRODUCT_NAME" ]] && [[ ":$BOARD_WIN3:" =~ "$BOARD_NAME" ]]; then
+if [[ ":$BOARD_WIN3:" =~ "$BOARD_NAME" ]]; then
   $DQ_PATH/gpd/win3/win3.sh
 
 # WinMax2
-elif [[ ":$PRODUCT_WM2:" == "$PRODUCT_NAME" ]] && [[ ":$BOARD_WM2:" =~ "$BOARD_NAME" ]]; then
+elif [[ ":$BOARD_WM2:" =~ "$BOARD_NAME" ]]; then
   $DQ_PATH/gpd/winmax2/winmax2.sh
 
 # No Match
