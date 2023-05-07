c86eb2efe53b77d59774181d6683157d78398f78
Matthew Anderson <ruinairas1992@gmail.com>
2023-05-07 14:38:44 -0500

    Fix logical errors and add debug notes to follow

--- usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus_dsdt.sh
+++ usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus_dsdt.sh
@@ -5,19 +5,28 @@ if [ $(whoami) != 'root' ]; then
    exit 1
 fi
 
-if ! grep -q 'acpi_override' /boot/loader/entries/frzr.conf
+if ! grep -q 'acpi_override' /boot/loader/entries/frzr.conf; then
 
    if [ ! -d /etc/kernel/firmware/acpi/ ]; then
       # create directory for acpi DSDT overrides
       mkdir -p /etc/kernel/firmware/acpi/
    fi
    
+   echo "Copying ACPI DSL to /etc/kernel/firmware/acpi/"
    cp /usr/lib/firmware/dsdt/ayaneo_air_plus.dsl /etc/kernel/firmware/acpi/dsdt.dsl
+   echo "Compiling DSDT..."
    iasl -tc /etc/kernel/firmware/acpi/dsdt.dsl
+   echo "Changing directory to /etc"
    cd /etc/
+   echo "Find kernel path and create acpi_override"
    find kernel | cpio -H newc --create > acpi_override
+   echo "Copy acpi_override to /boot"
    cp acpi_override /boot
+   echo "Add acpi_override to bootloader"
    sed -i 's#linux /vmlinuz-linux#&\ninitrd /acpi_override#' /boot/loader/entries/frzr.conf
+   echo "Done!"
+else
+   echo "acpi_override already detected in bootloader, exiting"
 fi
 
 
