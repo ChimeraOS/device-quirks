97357c0085ba9c94f710ca28a87c0399491daf88
Matthew Anderson <ruinairas1992@gmail.com>
2023-05-09 07:45:46 -0500

    Testing changes for fresh installs

--- usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus_dsdt.sh
+++ usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus_dsdt.sh
@@ -5,47 +5,37 @@ if [ $(whoami) != 'root' ]; then
    exit 1
 fi
 
-# If the script is being ran directly we will need to set the paths
 if [ -z "${MOUNT_PATH}" ]; then
-	if [ -d /tmp/frzr_root ]; then
-		MOUNT_PATH=/tmp/frzr_root/
-		echo "It appears as though we are installing from the install media"
-	elif [ -d /frzr_root ]; then
-		MOUNT_PATH=/frzr_root/
-		echo "It appears as though we are installing from a deployed system"
-	else
-		MOUNT_PATH=/
-		echo "It appears as though this script is being ran from a non-frzr based system"
-	fi
+	MOUNT_PATH=""
 fi
 
-ACPI_DIR="etc/kernel/firmware/acpi/"
-DEPLOYMENT_DSDT_PATH="${MOUNT_PATH}/usr/lib/firmware/dsdt/ayaneo_air_plus.dsl"
+if [ -z "${SUBVOL}" ]; then
+	SUBVOL=""
+fi
+
+ACPI_BUILD_DIR="/tmp/kernel/firmware/acpi"
+DEPLOYMENT_DSDT_PATH="${SUBVOL}/usr/lib/firmware/dsdt/ayaneo_air_plus.dsl"
 BOOTLOADER_CONFIG="${MOUNT_PATH}/boot/loader/entries/frzr.conf"
 ACPI_OVERRIDE_DEVICE="ayaneo_air_plus_IRQ_fix"
 
 if ! grep -q "${ACPI_OVERRIDE_DEVICE}" ${BOOTLOADER_CONFIG}; then
 
-   if [ ! -d "${MOUNT_PATH}${ACPI_DIR}" ]; then
+   if [ ! -d "${MOUNT_PATH}${ACPI_BUILD_DIR}" ]; then
       # create directory for acpi DSDT overrides
-      mkdir -p "${MOUNT_PATH}${ACPI_DIR}"
+      mkdir -p "${MOUNT_PATH}${ACPI_BUILD_DIR}"
    fi
    
-   echo "Copying ACPI DSL to /etc/kernel/firmware/acpi/"
-   cp ${DEPLOYMENT_DSDT_PATH} ${MOUNT_PATH} ${ACPI_DIR}/dsdt.dsl
+   cp ${DEPLOYMENT_DSDT_PATH} ${ACPI_BUILD_DIR}/dsdt.dsl
    echo "Compiling DSDT..."
-   iasl -tc ${ACPI_DIR}/dsdt.dsl
-   echo "Changing directory to /etc"
-   cd ${MOUNT_PATH}/etc/
-   echo "Find kernel path and create acpi_override"
+   iasl -tc ${ACPI_BUILD_DIR}/dsdt.dsl
+   cd ${MOUNT_PATH}/tmp/
    find kernel | cpio -H newc --create > ${ACPI_OVERRIDE_DEVICE}
-   echo "Copy acpi_override to /boot"
    cp ${ACPI_OVERRIDE_DEVICE} /boot
-   echo "Add acpi_override to bootloader"
+   echo "Adding $ACPI_OVERRIDE_DEVICE to bootloader"
    sed -i "s|linux .*/vmlinuz-linux|&\ninitrd /${ACPI_OVERRIDE_DEVICE}|" ${BOOTLOADER_CONFIG}
    echo "Done!"
 else
-   echo "acpi_override already detected in bootloader, exiting"
+   echo "$ACPI_OVERRIDE_DEVICE already detected in bootloader, exiting"
 fi
 
 
