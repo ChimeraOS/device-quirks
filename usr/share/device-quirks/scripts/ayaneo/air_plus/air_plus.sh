329b617ef460c56c4256b8229b783859f0676efc
Matthew Anderson <ruinairas1992@gmail.com>
2023-05-15 20:15:37 -0500

    Check for config in new image

--- usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus.sh
+++ usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus.sh
@@ -3,8 +3,13 @@ if [ $(whoami) != 'root' ]; then
 	echo "You must be root to run this script."
    exit 1
 fi
- 
-source /etc/device-quirks.conf
+
+if [ -d /tmp/frzr_root ]; then
+	source ${SUBVOL}/etc/device-quirks.conf
+else
+	source /etc/device-quirks.conf
+fi
+
 if [ $USE_FIRMWARE_OVERRIDES == '1' ]; then
   # Do EDID override.
   $DQ_PATH/scripts/ayaneo/air_plus/air_plus_dsdt.sh
