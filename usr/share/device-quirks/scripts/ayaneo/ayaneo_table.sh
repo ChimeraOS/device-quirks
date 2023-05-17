7b5b8f9012f7061500d939a2b6a918ab7caeaf16
Jerrod Frost <13701401+jcfrosty@users.noreply.github.com>
2023-05-16 21:04:56 -0500

    Update ayaneo_table.sh
    Add echos for verbose debugging.

--- usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
+++ usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
@@ -5,8 +5,10 @@ PRODUCT_LIST_2021="AYA NEO Founder:AYA NEO 2021:AYANEO 2021:AYANEO 2021 Pro:AYAN
 PRODUCT_LIST_AIR_PLUS="AIR Plus"
 
 if [[ ":$PRODUCT_LIST_2021:" =~ "$PRODUCT_NAME" ]]; then
+  echo "AYA NEO 2021 Device"
   $DQ_PATH/scripts/ayaneo/2021/2021.sh
 elif [[ ":$PRODUCT_LIST_AIR_PLUS:" =~ "$PRODUCT_NAME" ]]; then
+  "AYA NEO AIR PLUS"
   $DQ_PATH/scripts/ayaneo/air_plus/air_plus.sh
 else
   echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
