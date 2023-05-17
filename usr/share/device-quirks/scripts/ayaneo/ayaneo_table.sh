7ffe30a9f5a4fdba3c85b2237fcb76a847f86e63
Jerrod Frost <13701401+jcfrosty@users.noreply.github.com>
2023-05-16 21:47:09 -0500

    Update ayaneo_table.sh
    forgot echo

--- usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
+++ usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
@@ -8,7 +8,7 @@ if [[ ":$PRODUCT_LIST_2021:" =~ "$PRODUCT_NAME" ]]; then
   echo "AYA NEO 2021 Device"
   $DQ_PATH/scripts/ayaneo/2021/2021.sh
 elif [[ ":$PRODUCT_LIST_AIR_PLUS:" =~ "$PRODUCT_NAME" ]]; then
-  "AYA NEO AIR PLUS"
+  echo "AYA NEO AIR PLUS"
   $DQ_PATH/scripts/ayaneo/air_plus/air_plus.sh
 else
   echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
