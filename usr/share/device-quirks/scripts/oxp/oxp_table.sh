e54225246e0394b938769e93f252488ae49f10f4
Jerrod Frost <13701401+jcfrosty@users.noreply.github.com>
2023-05-16 21:02:58 -0500

    Update oxp_table.sh
    add echo for verbose debugging.

--- usr/share/device-quirks/scripts/oxp/oxp_table.sh
+++ usr/share/device-quirks/scripts/oxp/oxp_table.sh
@@ -3,6 +3,7 @@
 PRODUCT_NAME="$(cat /sys/devices/virtual/dmi/id/product_name)"
 
 if [[ "ONEXPLAYER Mini Pro" == "$PRODUCT_NAME" ]]; then
+    echo "OXP Mini Pro"
     $DQ_PATH/scripts/oxp/mini_pro/mini_pro.sh
     
 else
