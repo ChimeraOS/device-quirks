e0f95c76807dcd0c9c1de866e23771b300b5196d
Derek J. Clark <derekjohn.clark@gmail.com>
2023-05-23 08:04:19 -0700

    Remove unneeded source of config in ayaneo2 script

--- usr/share/device-quirks/scripts/ayaneo/2/2.sh
+++ usr/share/device-quirks/scripts/ayaneo/2/2.sh
@@ -4,12 +4,6 @@ if [ $(whoami) != 'root' ]; then
   exit 1
 fi
 
-if [ -d /tmp/frzr_root ]; then
-  source ${MOUNT_PATH}/etc/device-quirks.conf
-else
-  source /etc/device-quirks.conf
-fi
-
 # Force 16 bit audio, format S16LE, sample rate 96000.
 echo "Force S16LE 96000hz"
 $DQ_PATH/scripts/override_bitrate
