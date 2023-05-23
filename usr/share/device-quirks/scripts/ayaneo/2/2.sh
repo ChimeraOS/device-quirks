eb7b22d873ded41c0115ceea221b882afbfd70b7
Derek J. Clark <derekjohn.clark@gmail.com>
2023-05-23 08:01:51 -0700

    Add audio overrides for all currently supported 6800u handhelds

--- usr/share/device-quirks/scripts/ayaneo/2/2.sh
+++ usr/share/device-quirks/scripts/ayaneo/2/2.sh
@@ -3,7 +3,7 @@ if [ $(whoami) != 'root' ]; then
   echo "You must be root to run this script."
   exit 1
 fi
- 
+
 if [ -d /tmp/frzr_root ]; then
   source ${MOUNT_PATH}/etc/device-quirks.conf
 else
@@ -13,12 +13,3 @@ fi
 # Force 16 bit audio, format S16LE, sample rate 96000.
 echo "Force S16LE 96000hz"
 $DQ_PATH/scripts/override_bitrate
-
-if [[ $USE_FIRMWARE_OVERRIDES == 1 ]]; then
-  # Do DSDT override.
-  echo "Enabling DSDT Override"
-  $DQ_PATH/scriipts/override_dsdt "gpd_winmax2"
-else
-  echo "Firmware overrides are disabled, skipping...\n"
-  echo "To enable firmware overrides, edit /etc/device-quirks/device-quirks.conf"
-fi
--- usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
+++ usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
@@ -3,6 +3,7 @@
 PRODUCT_NAME="$(cat /sys/devices/virtual/dmi/id/product_name)"
 PRODUCT_LIST_2021="AYA NEO Founder:AYA NEO 2021:AYANEO 2021:AYANEO 2021 Pro:AYANEO 2021 Pro Retro Power"
 PRODUCT_LIST_AIR_PLUS="AIR Plus"
+PRODUCT_LIST_2="AYANEO 2:GEEK"
 
 if [[ ":$PRODUCT_LIST_2021:" =~ "$PRODUCT_NAME" ]]; then
   echo "AYA NEO 2021 Device"
@@ -10,6 +11,9 @@ if [[ ":$PRODUCT_LIST_2021:" =~ "$PRODUCT_NAME" ]]; then
 elif [[ ":$PRODUCT_LIST_AIR_PLUS:" =~ "$PRODUCT_NAME" ]]; then
   echo "AYA NEO AIR PLUS"
   $DQ_PATH/scripts/ayaneo/air_plus/air_plus.sh
+elif [[ ":$PRODUCT_LIST_2:" =~ "$PRODUCT_NAME" ]]; then
+  echo "AYA NEO 2 SERIES"
+  $DQ_PATH/scripts/ayaneo/2/2.sh
 else
   echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
 fi
