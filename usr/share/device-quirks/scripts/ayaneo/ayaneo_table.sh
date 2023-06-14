3f9e1e6f866e75ce07095d9eff13de8c90f5d0ab
Alesh Slovak <aleshslovak@gmail.com>
2023-06-14 08:31:15 -0400

    use exact match for device detection

--- usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
+++ usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
@@ -5,13 +5,13 @@ PRODUCT_LIST_2021="AYA NEO Founder:AYA NEO 2021:AYANEO 2021:AYANEO 2021 Pro:AYAN
 PRODUCT_LIST_AIR_PLUS="AIR Plus"
 PRODUCT_LIST_2="AYANEO 2:GEEK"
 
-if [[ ":$PRODUCT_LIST_2021:" =~ "$PRODUCT_NAME" ]]; then
+if [[ ":$PRODUCT_LIST_2021:" =~ ":$PRODUCT_NAME:" ]]; then
   echo "AYA NEO 2021 Device"
   $DQ_PATH/scripts/ayaneo/2021/2021.sh
-elif [[ ":$PRODUCT_LIST_AIR_PLUS:" =~ "$PRODUCT_NAME" ]]; then
+elif [[ ":$PRODUCT_LIST_AIR_PLUS:" =~ ":$PRODUCT_NAME:" ]]; then
   echo "AYA NEO AIR PLUS"
   $DQ_PATH/scripts/ayaneo/air_plus/air_plus.sh
-elif [[ ":$PRODUCT_LIST_2:" =~ "$PRODUCT_NAME" ]]; then
+elif [[ ":$PRODUCT_LIST_2:" =~ ":$PRODUCT_NAME:" ]]; then
   echo "AYA NEO 2 SERIES"
   $DQ_PATH/scripts/ayaneo/2/2.sh
 else
--- usr/share/device-quirks/scripts/gpd/gpd_table.sh
+++ usr/share/device-quirks/scripts/gpd/gpd_table.sh
@@ -12,14 +12,14 @@ BOARD_WM2="G1618-04"
 BOARD_WIN4="G1619-04"
 
 # WinMax2
-elif [[ ":$BOARD_WM2:" =~ "$BOARD_NAME" ]]; then
+if [[ ":$BOARD_WM2:" =~ ":$BOARD_NAME:" ]]; then
   echo "WinMax2"
   $DQ_PATH/scripts/gpd/winmax2/winmax2.sh
-elif [[ ":$BOARD_WIN4:" =~ "$BOARD_NAME" ]]; then
+elif [[ ":$BOARD_WIN4:" =~ ":$BOARD_NAME:" ]]; then
   echo "Win4"
   $DQ_PATH/scripts/gpd/win4/win4.sh
 
 # No Match
 else
-  echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
+  echo "${BOARD_NAME} does not have a quirk configuration script. Exiting."
 fi
