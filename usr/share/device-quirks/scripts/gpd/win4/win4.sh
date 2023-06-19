612cc7b33b8300e0ca9c91af5c1c0c9e77725e00
Derek J. Clark <derekjohn.clark@gmail.com>
2023-06-19 13:46:01 -0700

    Fix error message when firmware overrides are not enabled to use the correct path.

--- usr/share/device-quirks/scripts/gpd/win4/win4.sh
+++ usr/share/device-quirks/scripts/gpd/win4/win4.sh
@@ -20,5 +20,5 @@ if [[ $USE_FIRMWARE_OVERRIDES == 1 ]]; then
   $DQ_PATH/scripts/override_edid "eDP-1" "gpd_win4_edid.bin"
 else
   echo "Firmware overrides are disabled, skipping...\n"
-  echo "To enable firmware overrides, edit /etc/device-quirks/device-quirks.conf"
+  echo "To enable firmware overrides, edit /etc/device-quirks.conf"
 fi
