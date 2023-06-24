f2e4cf565d725e9e650ac9a452b017c5addb65de
Derek J. Clark <derekjohn.clark@gmail.com>
2023-06-24 12:13:53 -0700

    Switch to checksum match instead of DMI match

--- usr/share/device-quirks/scripts/ayaneo/2021/2021.sh
+++ usr/share/device-quirks/scripts/ayaneo/2021/2021.sh
@@ -13,16 +13,14 @@ fi
 PRODUCT_NAME="$(cat /sys/devices/virtual/dmi/id/product_name)"
 if [[ $USE_FIRMWARE_OVERRIDES == 1 ]]; then
   # Do DSDT override.
-  case $PRODUCT_NAME in
-    "AYANEO 2021")
-      echo "Enabling DSDT Override"
-      $DQ_PATH/scripts/override_dsdt "ayaneo_2021.dsl"
-      ;;
-    "AYANEO 2021 Pro Retro Power")
-      echo "Enabling DSDT Override"
-      $DQ_PATH/scripts/override_dsdt "ayaneo_2021_pro_retro_power.dsl"
-      ;;
-  esac
+  DSDT_OVERRIDES="ayaneo_2021_0x03.dsl ayaneo_2021_0xE3.dsl"
+  for dsdt in $DSDT_OVERRIDES; do
+    APPLY_PATCH=$($DQ_PATH/scripts/verify_dsdt $dsdt)
+    if [[ $APPLY_PATCH == 1 ]]; then
+      $DQ_PATH/scripts/override_dsdt $dsdt
+      break
+    fi
+  done
 else
   echo "Firmware overrides are disabled, skipping...\n"
   echo "To enable firmware overrides, edit /etc/device-quirks/device-quirks.conf"
