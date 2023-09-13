d01d3c81f48a9a8c702f5cb77a8dd5a179f22039
Derek J. Clark <derekjohn.clark@gmail.com>
2023-09-12 17:03:01 -0700

    Remove outdated dsdt overrides.

--- usr/share/device-quirks/scripts/gpd/win4/win4.sh
+++ usr/share/device-quirks/scripts/gpd/win4/win4.sh
@@ -13,12 +13,3 @@ fi
 # Force 16 bit audio, format S16LE, sample rate 96000.
 echo "Force S16LE 96000hz"
 $DQ_PATH/scripts/override_bitrate
-
-if [[ $USE_FIRMWARE_OVERRIDES == 1 ]]; then
-  # Do EDID override.
-  echo "Enabling EDID Override"
-  $DQ_PATH/scripts/override_edid "eDP-1" "gpd_win4_edid.bin"
-else
-  echo "Firmware overrides are disabled, skipping...\n"
-  echo "To enable firmware overrides, edit /etc/device-quirks/device-quirks.conf"
-fi
