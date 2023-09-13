14ab002cd125bd5373919ef6881b700ef85a6957
Derek J. Clark <derekjohn.clark@gmail.com>
2023-09-12 17:03:01 -0700

    Remove outdated dsdt overrides.

--- usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus.sh
+++ usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus.sh
@@ -13,21 +13,3 @@ fi
 # Force 16 bit audio, format S16LE, sample rate 96000.
 echo "Force S16LE 96000hz"
 $DQ_PATH/scripts/override_bitrate
-
-if [[ $USE_FIRMWARE_OVERRIDES == 1 ]]; then
-  BIOS="$(cat /sys/devices/virtual/dmi/id/bios_version)"
-  if [[ $BIOS == "E.AB05_A_V.D16..006" ]]; then
-    echo "Enabling DSDT Override for D16 BIOS."
-    DSDT_OVERRIDES="ayaneo_air_plus_D16_0x0D.dsl ayaneo_air_plus_D16_0x0F.dsl ayaneo_air_plus_D16_0x7D.dsl ayaneo_air_plus_D16_0xCF.dsl"
-  elif [[ $BIOS == "E.AB05_A_V.D32..006" ]]; then
-    echo "Enabling DSDT Override for D32 BIOS."
-    DSDT_OVERRIDES="ayaneo_air_plus_D32_0x0D.dsl ayaneo_air_plus_D32_0x94.dsl ayaneo_air_plus_D32_0xD4.dsl"
-  else
-    echo "No matching BIOS found. DSDT Override skipped."
-  fi
-
-  $DQ_PATH/scripts/override_dsdt $DSDT_OVERRIDES
-else
-  echo "Firmware overrides are disabled, skipping...\n"
-  echo "To enable firmware overrides, edit /etc/device-quirks/device-quirks.conf"
-fi
