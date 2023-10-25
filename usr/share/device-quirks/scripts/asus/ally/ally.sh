78283e702eacbd72df118a1dff57e931861903fa
game-de-it <game.de.it.0@gmail.com>
2023-10-25 12:54:04 +0900

    Use equalizer with ROG Ally

---
+++ usr/share/device-quirks/scripts/asus/ally/ally.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $(whoami) != 'root' ]; then
+  echo "You must be root to run this script."
+  exit 1
+fi
+
+if [ -d /tmp/frzr_root ]; then
+  source ${MOUNT_PATH}/etc/device-quirks/device-quirks.conf
+else
+  source /etc/device-quirks/device-quirks.conf
+fi
+
+# Create an equalizer for pipwier.
+mkdir -p ~/.config/pipewire/pipewire.conf.d
+cp $DQ_PATH/scripts/asus/ally/sink-eq6.conf ~/.config/pipewire/pipewire.conf.d/sink-eq6.conf
