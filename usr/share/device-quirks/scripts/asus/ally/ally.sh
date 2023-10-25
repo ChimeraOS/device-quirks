37119642f0cc51590347fbcf0217ff03785db483
game-de-it <game.de.it.0@gmail.com>
2023-10-25 13:57:10 +0900

    Typoed pipewire

--- usr/share/device-quirks/scripts/asus/ally/ally.sh
+++ usr/share/device-quirks/scripts/asus/ally/ally.sh
@@ -10,6 +10,6 @@ else
   source /etc/device-quirks/device-quirks.conf
 fi
 
-# Create an equalizer for pipwier.
+# Create an equalizer for pipewire.
 mkdir -p ~/.config/pipewire/pipewire.conf.d
 cp $DQ_PATH/scripts/asus/ally/sink-eq6.conf ~/.config/pipewire/pipewire.conf.d/sink-eq6.conf
