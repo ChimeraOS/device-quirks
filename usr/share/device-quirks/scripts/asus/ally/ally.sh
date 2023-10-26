d88aa67dc53dda718b5861afae3d798c671e61cd
game-de-it <game.de.it.0@gmail.com>
2023-10-26 13:54:25 +0900

    Removed firmware override processing

--- usr/share/device-quirks/scripts/asus/ally/ally.sh
+++ usr/share/device-quirks/scripts/asus/ally/ally.sh
@@ -4,12 +4,6 @@ if [ $(whoami) != 'root' ]; then
   exit 1
 fi
 
-if [ -d /tmp/frzr_root ]; then
-  source ${MOUNT_PATH}/etc/device-quirks/device-quirks.conf
-else
-  source /etc/device-quirks/device-quirks.conf
-fi
-
 # Create an equalizer for pipewire.
 mkdir -p ~/.config/pipewire/pipewire.conf.d
 cp $DQ_PATH/scripts/asus/ally/sink-eq6.conf ~/.config/pipewire/pipewire.conf.d/sink-eq6.conf
