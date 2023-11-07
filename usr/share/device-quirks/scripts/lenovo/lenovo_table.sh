daf1b8edbe326d34b09bd4fededba384edc2a0f7
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-11-06 18:48:36 -0800

    Update lenovo_table.sh
    update legion-go.sh title

--- usr/share/device-quirks/scripts/lenovo/lenovo_table.sh
+++ usr/share/device-quirks/scripts/lenovo/lenovo_table.sh
@@ -6,7 +6,7 @@ CPU_NAME="$(cat /proc/cpuinfo | awk 'NR==5 {print $4$5$6$7}')"
 
 if [[ "83E1" == "$PRODUCT_NAME" ]] && [[ "LNVNB161216" == "$BOARD_NAME" ]] && [[ "AMDRyzenZ1Extreme" == "$CPU_NAME" ]]; then
     echo "Legion Go detected"
-    $DQ_PATH/scripts/lenovo/legion/legion.sh
+    $DQ_PATH/scripts/lenovo/legion-go/legion-go.sh
 
 else
     echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
