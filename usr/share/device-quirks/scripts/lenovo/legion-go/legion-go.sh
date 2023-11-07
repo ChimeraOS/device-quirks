e7c67523621208c9ae4089d20da4a0c2a20c2f56
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-11-06 18:50:14 -0800

    Update legion-go.sh
    get rid of redundant error

--- usr/share/device-quirks/scripts/lenovo/legion-go/legion-go.sh
+++ usr/share/device-quirks/scripts/lenovo/legion-go/legion-go.sh
@@ -1,17 +1,3 @@
-#/bin/bash
-
-PRODUCT_NAME="$(cat /sys/devices/virtual/dmi/id/product_name)"
-BOARD_NAME="$(cat /sys/devices/virtual/dmi/id/board_name)"
-CPU_NAME="$(cat /proc/cpuinfo | awk 'NR==5 {print $4$5$6$7}')"
-
-if [[ "83E1" == "$PRODUCT_NAME" ]] && [[ "LNVNB161216" == "$BOARD_NAME" ]] && [[ "AMDRyzenZ1Extreme" == "$CPU_NAME" ]]; then
-    echo "Legion Go detected"
-    $DQ_PATH/scripts/lenovo/legion/legion.sh
-
-else
-    echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
-fi
-
 # Create the base directory and subdirectories for easyeffects
 mkdir -p "$HOME/.config/easyeffects"
 mkdir -p "$HOME/.config/easyeffects/autoload"
