394108aff56f74db9fd075e743eccb2d5c9da8b9
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-11-06 19:36:58 -0800

    Update ally.sh
    adds Ally easyeffects surround virtualization config files

--- usr/share/device-quirks/scripts/asus/ally/ally.sh
+++ usr/share/device-quirks/scripts/asus/ally/ally.sh
@@ -4,6 +4,42 @@ if [ $(whoami) != 'root' ]; then
   exit 1
 fi
 
-# Create an equalizer for pipewire.
-mkdir -p ~/.config/pipewire/pipewire.conf.d
-cp $DQ_PATH/scripts/asus/ally/sink-eq6.conf ~/.config/pipewire/pipewire.conf.d/sink-eq6.conf
+# Create the base directory and subdirectories for easyeffects
+mkdir -p "$HOME/.config/easyeffects"
+mkdir -p "$HOME/.config/easyeffects/autoload"
+mkdir -p "$HOME/.config/easyeffects/autoload/input"
+mkdir -p "$HOME/.config/easyeffects/autoload/output"
+mkdir -p "$HOME/.config/easyeffects/irs"
+mkdir -p "$HOME/.config/easyeffects/input"
+mkdir -p "$HOME/.config/easyeffects/output"
+
+# Define the target directories
+output_dir="$HOME/.config/easyeffects/output"
+autoload_output_dir="$HOME/.config/easyeffects/autoload/output"
+irs_dir="$HOME/.config/easyeffects/irs"
+
+# Define the path of the script's current location (directory of this script)
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Copy the necessary files to the target directories
+cp "$script_dir/alsa_output.pci-0000_c2_00.6.analog-stereoanalog-output-speaker.json" "$autoload_output_dir/alsa_output.pci-0000_c2_00.6.analog-stereoanalog-output-speaker.json"
+cp "$script_dir/game.irs" "$irs_dir/game.irs"
+
+# Generate the dolbers-atomsph.json file with the correct kernel-path
+dolbers_atomsph_path="$output_dir/dolbers-atomsph.json"
+echo '{
+    "output": {
+        "blocklist": [],
+        "convolver#0": {
+            "autogain": true,
+            "bypass": false,
+            "input-gain": 0.0,
+            "ir-width": 100,
+            "kernel-path": "'"${irs_dir}/game.irs"'"',
+            "output-gain": 0.0
+        },
+        "plugins_order": [
+            "convolver#0"
+        ]
+    }
+}' > "$dolbers_atomsph_path"
