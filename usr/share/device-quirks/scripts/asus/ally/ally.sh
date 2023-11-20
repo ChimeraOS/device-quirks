c7a185564198fa8b7b4b8f4b2a787fb5b3c6d04c
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-11-19 22:49:27 -0800

    Update ally.sh

--- usr/share/device-quirks/scripts/asus/ally/ally.sh
+++ usr/share/device-quirks/scripts/asus/ally/ally.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 
-# Find the main user's home directory under /home/
-# Assumes the main user has the lowest UID greater than 1000
-MAIN_USER_HOME=$(awk -F':' '{ if ($3 >= 1000 && $6 ~ /^\/home\//) print $0 }' /etc/passwd | sort -t: -k3,3n | head -n 1 | cut -d: -f6)
+# Check for root
+if [ "$(whoami)" != 'root' ]; then
+  echo "You must be root to run this script."
+  exit 1
+fi
 
-# Replace the \$HOME with the detected main user's home directory
-# Create the base directory and subdirectories for easyeffects
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/autoload"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/autoload/input"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/autoload/output"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/irs"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/input"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/output"
+# Define the path of the legion-go.sh script and pipewire folder
+ally_dir="$DQ_PATH/scripts/asus/ally"
+pipewire_dir="$ally_dir/pipewire"  # Adjust this path as necessary
 
-# Define the target directories
-output_dir=""$MAIN_USER_HOME"/.config/easyeffects/output"
-irs_dir=""$MAIN_USER_HOME"/.config/easyeffects/irs"
-
-# Define the path of the script's current location (directory of this script)
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# Copy the necessary files to the target directories
-cp "$script_dir/game.irs" "$irs_dir/game.irs"
-
-# Generate the dolbers-atomsph.json file with the correct kernel-path
-dolbers_atomsph_path="$output_dir/dolbers-atomsph.json"
-echo '{
-    "output": {
-        "blocklist": [],
-        "convolver#0": {
-            "autogain": true,
-            "bypass": false,
-            "input-gain": 0.0,
-            "ir-width": 100,
-            "kernel-path": "'"${irs_dir}/game.irs"'"',
-            "output-gain": 0.0
-        },
-        "plugins_order": [
-            "convolver#0"
-        ]
-    }
-}' > "$dolbers_atomsph_path"
+# Check if the PipeWire directory exists
+if [ -d "$pipewire_dir" ]; then
+    # Copy the PipeWire folder to /etc
+    cp -r "$pipewire_dir" /etc/
+else
+    echo "PipeWire directory not found in $pipewire_dir."
+    exit 1
+fi
