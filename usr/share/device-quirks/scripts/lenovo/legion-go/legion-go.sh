6ef73a250f04cccc5ac4e8782ccc503c43257b94
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2023-11-19 22:42:01 -0800

    Update legion-go.sh
    goodbye easyeffects + $HOME directory involvement, hello /etc and simplified pipewire filter-chain

--- usr/share/device-quirks/scripts/lenovo/legion-go/legion-go.sh
+++ usr/share/device-quirks/scripts/lenovo/legion-go/legion-go.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-# Find the main user's home directory under /home/
-# Assumes the main user has the lowest UID greater than 1000
-MAIN_USER_HOME=$(awk -F':' '{ if ($3 >= 1000 && $6 ~ /^\/home\//) print $0 }' /etc/passwd | sort -t: -k3,3n | head -n 1 | cut -d: -f6)
+# Check for root
+if [ "$(whoami)" != 'root' ]; then
+  echo "You must be root to run this script."
+  exit 1
+fi
 
-# Replace the \$HOME with the detected main user's home directory
-# Create the base directory and subdirectories for easyeffects
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/autoload"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/autoload/input"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/autoload/output"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/input"
-mkdir -p ""$MAIN_USER_HOME"/.config/easyeffects/output"
+# Define the path of the legion-go.sh script and pipewire folder
+legion_go_dir="$DQ_PATH/scripts/lenovo/legion-go"
+pipewire_dir="$legion_go_dir/pipewire"  # Adjust this path as necessary
 
-# Define the target directories
-output_dir=""$MAIN_USER_HOME"/.config/easyeffects/output"
-
-# Create the directories if they don't exist
-mkdir -p "$output_dir"
-
-# Define the path of the script's current location (directory of this script)
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# Copy the files to the target directories
-cp "$script_dir/dolbers-atomsph.json" "$output_dir/dolbers-atomsph.json"
+# Check if the PipeWire directory exists
+if [ -d "$pipewire_dir" ]; then
+    # Copy the PipeWire folder to /etc
+    cp -r "$pipewire_dir" /etc/
+else
+    echo "PipeWire directory not found in $pipewire_dir."
+    exit 1
+fi
