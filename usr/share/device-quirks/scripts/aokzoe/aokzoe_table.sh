540c6be0f8b2e72edca0344f30947099f8132995
Jerrod Frost <13701401+jcfrosty@users.noreply.github.com>
2023-12-08 16:47:30 -0600

    Update aokzoe_table.sh

--- usr/share/device-quirks/scripts/aokzoe/aokzoe_table.sh
+++ usr/share/device-quirks/scripts/aokzoe/aokzoe_table.sh
@@ -6,7 +6,7 @@ if [[ "AOKZOE A1 AR07" == "$PRODUCT_NAME" ]]; then
     echo "A1 AR07"
     $DQ_PATH/scripts/aokzoe/a1/a1.sh
 elif [[ "AOKZOE A1 Pro" == "$PRODUCT_NAME" ]]; then
-    $DQ_PATH/scripts/aokzoe/a1/a1pro.sh
+    $DQ_PATH/scripts/aokzoe/a1/a1.sh
 else
     echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
 fi
