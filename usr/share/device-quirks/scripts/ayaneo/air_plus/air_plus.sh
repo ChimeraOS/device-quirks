2cde4814e1cc51b656d55fb1441902ebc9ef7fb1
ヱッジ <uejjik@gmail.com>
2024-01-07 18:51:29 -0600

    Air Plus EDID override

--- usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus.sh
+++ usr/share/device-quirks/scripts/ayaneo/air_plus/air_plus.sh
@@ -13,3 +13,12 @@ fi
 # Force 16 bit audio, format S16LE, sample rate 96000.
 echo "Force S16LE 96000hz"
 $DQ_PATH/scripts/override_bitrate
+
+if [[ $USE_FIRMWARE_OVERRIDES == 1 ]]; then
+  # Do EDID override.
+  echo "Enabling EDID Override"
+  $DQ_PATH/scripts/override_edid "eDP-1" "ayaneo_airplus_edid.bin"
+else
+  echo "Firmware overrides are disabled, skipping...\n"
+  echo "To enable firmware overrides, edit /etc/device-quirks/device-quirks.conf"
+fi
