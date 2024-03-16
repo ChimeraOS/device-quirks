290db542dade385ac721249e19366f23d01ec3e2
honjow <honjow311@gmail.com>
2024-03-16 12:13:00 +0800

    fix(Ally):Resolve suspend issues

--- usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
+++ usr/lib/systemd/system-sleep/systemd-suspend-mods.sh
@@ -5,6 +5,13 @@
 
 MOD_LIST=$(grep -v ^\# /etc/device-quirks/systemd-suspend-mods.conf)
 
+PRODUCT=$(cat /sys/devices/virtual/dmi/id/product_name)
+ROG_LIST="ROG Ally RC71L_RC71L:ROG Ally RC71L"
+
+if [[ ":$ROG_LIST:" =~ ":$PRODUCT:" ]]; then
+    exit 0
+fi
+
 case $1 in
     pre)
         for mod in $MOD_LIST; do
