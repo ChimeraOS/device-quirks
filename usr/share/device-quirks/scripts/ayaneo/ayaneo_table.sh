fc11621b07e48962679c4ca14947b77e6bbd926e
Derek J. Clark <derekjohn.clark@gmail.com>
2024-04-28 18:00:21 -0700

    feat(Awinic): Add aw87xxx firmware and left speaker loading.

--- usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
+++ usr/share/device-quirks/scripts/ayaneo/ayaneo_table.sh
@@ -1,15 +1,27 @@
 #/bin/bash
 
 PRODUCT_NAME="$(cat /sys/devices/virtual/dmi/id/product_name)"
+PRODUCT_LIST_AIR_1S="AIR 1S: AIR 1S Limited"
 PRODUCT_LIST_AIR_PLUS="AIR Plus"
-PRODUCT_LIST_2="AYANEO 2:GEEK"
+PRODUCT_LIST_2="AYANEO 2:AYANEO 2S:GEEK:GEEK 1S"
+PRODUCT_LIST_FLIP="FLIP KB:FLIP DS"
+PRODUCT_LIST_KUN="AYANEO KUN"
 
-if [[ ":$PRODUCT_LIST_AIR_PLUS:" =~ ":$PRODUCT_NAME:" ]]; then
+if [[ ":$PRODUCT_LIST_AIR_1S:" =~ ":$PRODUCT_NAME:" ]]; then
+	echo "AYA NEO AIR 1S"
+	$DQ_PATH/scripts/ayaneo/air_1s/air_1s.sh
+elif [[ ":$PRODUCT_LIST_AIR_PLUS:" =~ ":$PRODUCT_NAME:" ]]; then
 	echo "AYA NEO AIR PLUS"
 	$DQ_PATH/scripts/ayaneo/air_plus/air_plus.sh
 elif [[ ":$PRODUCT_LIST_2:" =~ ":$PRODUCT_NAME:" ]]; then
 	echo "AYA NEO 2 SERIES"
 	$DQ_PATH/scripts/ayaneo/2/2.sh
+elif [[ ":$PRODUCT_LIST_FLIP:" =~ ":$PRODUCT_NAME:" ]]; then
+	echo "AYA NEO FLIP"
+	$DQ_PATH/scripts/ayaneo/flip/flip.sh
+elif [[ ":$PRODUCT_LIST_KUN:" =~ ":$PRODUCT_NAME:" ]]; then
+	echo "AYA NEO AIR PLUS"
+	$DQ_PATH/scripts/ayaneo/kun/kun.sh
 else
 	echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
 fi
---
+++ usr/share/device-quirks/scripts/ayaneo/kun/kun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $(whoami) != 'root' ]; then
+	echo "You must be root to run this script."
+	exit 1
+fi
+
+if [ -d /tmp/frzr_root ]; then
+	source ${MOUNT_PATH}/etc/device-quirks/device-quirks.conf
+else
+	source /etc/device-quirks/device-quirks.conf
+fi
+
+# Force 16 bit audio, format S16LE, sample rate 96000.
+echo "Force S16LE 96000hz"
+$DQ_PATH/scripts/override_bitrate
