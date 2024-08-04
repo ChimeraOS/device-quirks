f814dc239ae917a7def6ce45523221b0e7ce6cf7
Derek J. Clark <derekjohn.clark@gmail.com>
2024-08-03 17:25:03 -0700

    fix(Audio): Apply latest convolver scripts for ROG Ally and Legion Go.

--- usr/share/device-quirks/scripts/asus/asus_table.sh
+++ usr/share/device-quirks/scripts/asus/asus_table.sh
@@ -3,8 +3,11 @@ BOARD_NAME="$(cat /sys/devices/virtual/dmi/id/board_name)"
 CPU_NAME="$(cat /proc/cpuinfo | awk 'NR==5 {print $4$5$6$7}')"
 
 if [[ "RC71L" == "$BOARD_NAME" ]] && [[ "AMDRyzenZ1Extreme" == "$CPU_NAME" ]]; then
-	echo "ROG Ally detected"
-	$DQ_PATH/scripts/asus/ally/ally.sh
+  echo "ROG Ally detected"
+  $DQ_PATH/scripts/asus/rog-ally/rog-ally.sh
+elif [[ "RC72LA" == "$BOARD_NAME" ]] && [[ "AMDRyzenZ1Extreme" == "$CPU_NAME" ]]; then
+  echo "ROG Ally X detected"
+  $DQ_PATH/scripts/asus/rog-ally-x/rog-ally-x.sh
 else
-	echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
+  echo "${PRODUCT_NAME} does not have a quirk configuration script. Exiting."
 fi
