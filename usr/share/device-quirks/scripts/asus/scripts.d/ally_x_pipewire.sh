4f9597e2f2c9e467b335b2f91aa3bcb8590eea70
Derek J. Clark <derekjohn.clark@gmail.com>
2024-08-25 16:06:31 -0700

    Update MT79 and pipewire quirks to latest versions.

--- usr/share/device-quirks/scripts/asus/scripts.d/ally_x_pipewire.sh
+++ usr/share/device-quirks/scripts/asus/scripts.d/ally_x_pipewire.sh
@@ -2,34 +2,34 @@
 
 # Returning a unique id to allow users to toggle on or off in /etc/device-quirks/device-quirks.conf
 device_quirk_id(){
-    echo "ALLY-PIPEWIRE-CFG"
+    echo "ALLY-X-PIPEWIRE-CFG"
 }
 
 # Returning the name of the fix to display.
 device_quirk_name(){
-    echo "ROG Ally Pipewire Configuration"
+    echo "ROG Ally X Pipewire Configuration"
 }
 PIPEWIRE_DIR=${DQ_WORKING_PATH}/etc/pipewire
-ALLY_PATH=${DQ_PATH}/scripts/asus/resources/ALLY-PIPEWIRE-CFG/pipewire
-ALLY_PIPEWIRE_DIR=${DQ_PATH}/scripts/asus/resources/ALLY-PIPEWIRE-CFG/pipewire
+ALLY_X_PATH=${DQ_PATH}/scripts/asus/resources/ALLY-X-PIPEWIRE-CFG/pipewire
+ALLY_X_PIPEWIRE_DIR=${DQ_PATH}/scripts/asus/resources/ALLY-X-PIPEWIRE-CFG/pipewire
 # Do the install here.
 device_quirk_install(){
 
-  if [[ -d "${PIPEWIRE_DIR}" ]] && [[ ! -z $(diff -qr "${PIPEWIRE_DIR}" "${ALLY_PIPEWIRE_DIR}") ]]; then
+  if [[ -d "${PIPEWIRE_DIR}" ]] && [[ ! -z $(diff -qr "${PIPEWIRE_DIR}" "${ALLY_X_PIPEWIRE_DIR}") ]]; then
         return 2
-    elif [[ -d ${PIPEWIRE_DIR} ]] && [[ ! -e ${ALLY_PATH} ]]; then
+    elif [[ -d ${PIPEWIRE_DIR} ]] && [[ ! -e ${ALLY_X_PATH} ]]; then
         echo "There is no easy way to merge configuration files for Pipewire, renaming existing ${PIPEWIRE_DIR} directory to pipewire.bak."
         mv "${PIPEWIRE_DIR}" "${PIPEWIRE_DIR}.bak"
         if [ $? -eq 0 ]; then
-            cp -r "${ALLY_PIPEWIRE_DIR}" "${DQ_WORKING_PATH}/etc/"
-            touch "${PIPEWIRE_DIR}/.ALLY-PIPEWIRE-CFG"
+            cp -r "${ALLY_X_PIPEWIRE_DIR}" "${DQ_WORKING_PATH}/etc/"
+            touch "${PIPEWIRE_DIR}/.ALLY-X-PIPEWIRE-CFG"
             return 0
         else
             echo "Error renaming existing pipewire directory."
             return 1
         fi
     else
-        cp -r "${ALLY_PIPEWIRE_DIR}" "${DQ_WORKING_PATH}/etc/"
+        cp -r "${ALLY_X_PIPEWIRE_DIR}" "${DQ_WORKING_PATH}/etc/"
         if [ $? -eq 0 ]; then
             return 0
         else
@@ -42,7 +42,7 @@ device_quirk_install(){
 # Remove the install here.
 device_quirk_removal(){
 
-    if [[ -d "${PIPEWIRE_DIR}" ]] && [[ -z $(diff -qr "${PIPEWIRE_DIR}" "${ALLY_PIPEWIRE_DIR") ]]; then
+    if [[ -d "${PIPEWIRE_DIR}" ]] && [[ -z $(diff -qr "${PIPEWIRE_DIR}" "${ALLY_X_PIPEWIRE_DIR") ]]; then
         rm -rf "${DQ_WORKING_PATH}/etc/pipewire"
         if [ $? -eq 0 ]; then
             return 0
---
+++ usr/share/device-quirks/scripts/chips/scripts.d/MT7921E.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# File constants
+DEPLOYED_FILE=${$DQ_WORKING_PATH}/etc/device-quirks/systemd-suspend-mods.conf
+SOURCE_FILE=${DQ_PATH}/scripts/resources/systemd-suspend-mods.conf
+
+# Returning a unique id to allow users to toggle on or off in /etc/device-quirks/device-quirks.conf
+device_quirk_id(){
+    echo "MT7921E"
+}
+
+# Returning the name of the fix to display.
+device_quirk_name(){
+    echo "Add MT7921E fix"
+}
+
+# Do the install here.
+device_quirk_install(){
+    # On new installs, we need to add the file so the quirk exists
+	  if [[ ! -f ${DEPLOYED_FILE} ]]; then
+        DIR_NAME=$(dirname ${DEPLOYED_FILE})
+        if [[ ! -d ${DIR_NAME} ]]; then
+            echo "Creating device_quirks location."
+            mkdir -p ${DIR_NAME}
+        fi
+        # Create device-quirks with mt7921e config
+        echo "Creating empty default config"
+        cat >${DEPLOYED_FILE} <<EOL
+# Line separated list of modules to unload/reload at suspend/resume.
+mt7921e
+EOL
+    fi
+
+    # Nothing to do if the qurik exists
+    if [[ $(grep -i 'mt7921e' ${DEPLOYED_FILE} ]]; then
+        return 0
+    # Add the mt7921e quirk if it doesnt exist
+    elif [[ ! $(grep -i 'mt7921e' ${DEPLOYED_FILE} ]]; then
+        echo "mt7921e"" >> ${DEPLOYED_FILE}
+        return 0
+    # Error if we failed to apply the quirk
+	  fi
+        return 2
+}
+
+# Remove the install here.
+device_quirk_removal(){
+    # Remove the MT7921E quirk
+    if [[ -f ${DEPLOYED_FILE} ]] && [ $(grep -i 'mt7921e' ${DEPLOYED_FILE} ]; then
+        grep -iv 'mt7921e' ${DEPLOYED_FILE} > ${DEPLOYED_FILE}
+        return 0
+    elif [ -f ${DEPLOYED_FILE} ]; then
+        return 2
+    fi
+}
